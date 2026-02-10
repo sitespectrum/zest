872dae8951d4476d943649bbe3d6c39a6ad94b0d
akosfodor <akosfodor10@gmail.com>
2026-02-10 13:13:20 +0100

    custom components wip

--- client/lib/components/topo_background.dart
+++ client/lib/components/topo_background.dart
@@ -1,10 +1,8 @@
 import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:functional_widget_annotation/functional_widget_annotation.dart';
-import '';
 part "topo_background.g.dart";
 
 @hwidget
