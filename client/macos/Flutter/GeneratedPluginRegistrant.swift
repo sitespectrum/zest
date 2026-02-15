80a51c61309facb276bea049af614b6867f14a93
SledDev <sleddev@asd.dog>
2026-02-15 17:09:37 +0100

    removed unused packages

--- client/macos/Flutter/GeneratedPluginRegistrant.swift
+++ client/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,21 +6,15 @@ import FlutterMacOS
 import Foundation
 
 import file_selector_macos
-import firebase_auth
-import firebase_core
 import flutter_ble_peripheral
 import flutter_blue_plus_darwin
-import google_sign_in_ios
 import mobile_scanner
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
-  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
-  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FlutterBlePeripheralPlugin.register(with: registry.registrar(forPlugin: "FlutterBlePeripheralPlugin"))
   FlutterBluePlusPlugin.register(with: registry.registrar(forPlugin: "FlutterBluePlusPlugin"))
-  FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
